5167ed74dd83e8795eefc19bcc32878cdc4a34e3
John Hawkins <hawkjo@gmail.com>
2023-11-10 15:26:06 +0100

    Added directional UMI collapsing

--- SDRranger/umi.py
+++ SDRranger/umi.py
@@ -78,3 +78,90 @@ def get_umi_maps_from_bam_file(
         umi_map_given_bc[bc] = get_umi_map_from_cntr(umi_cntr, max_dist=max_dist, dist_type=dist_type)
 
     return umi_map_given_bc
+
+def get_directional_connected_components(
+        umis: list, 
+        umi_cntr: Counter,
+        max_dist: int = 1,
+        dist_type: str = 'hamming'
+        ) -> Tuple[int, np.array]:
+    """
+    Builds adjacency matrix of umis given max_dist and calls connected_componenets
+
+    returns
+        :int:       n_vals
+        :int_array: component_array
+    """
+    if dist_type == 'hamming':
+        dist_func = simple_hamming_distance
+    elif dist_type == 'edit':
+        dist_func = editdistance.distance
+    else:
+        raise ValueError('dist type must be either hamming or edit')
+
+
+    # Build direction-based adjacency matrix: i->j if cnt_i >= 2*cnt_j - 1 as in umitools
+    adj_mat = lil_matrix((len(umis), len(umis)), dtype=np.uint8)
+    for i, umi_i in enumerate(umis):
+        umi_i_cnt = umi_cntr[umi_i]
+        for j in range(i+1, len(umis)):
+            umi_j = umis[j]
+            umi_j_cnt = umi_cntr[umi_j]
+            i_to_j = (umi_i_cnt >= 2*umi_j_cnt - 1)
+            j_to_i = (umi_j_cnt >= 2*umi_i_cnt - 1)
+            if (i_to_j or j_to_i) and dist_func(umi_i, umi_j) <= max_dist:
+                if i_to_j:
+                    adj_mat[i, j] = 1
+                else:  # j_to_i
+                    adj_mat[j, i] = 1
+
+    # Only allow one incoming edge per node. Keep max count or random if equal.
+    nonzero_rows_given_col = defaultdict(list)
+    for i, j in zip(*adj_mat.nonzero()):
+        nonzero_rows_given_col[j].append(i)
+    for j, i_list in nonzero_rows_given_col.items():
+        if len(i_list) > 1:
+            max_i = max(i_list, key=lambda i: umi_cntr[umis[i]])
+            for i in i_list:
+                if i != max_i:
+                    adj_mat[i, j] = 0
+
+    return connected_components(adj_mat)
+
+def get_directional_umi_map_from_cntr(umi_cntr: Counter, max_dist: int = 1, dist_type: str = 'hamming') -> dict:
+    umi_list = list(umi_cntr.keys())
+    n_vals, component_array = get_directional_connected_components(umi_list, umi_cntr, max_dist=max_dist, dist_type=dist_type)
+    component_max_umi = [None for _ in range(n_vals)]
+    for umi, component in zip(umi_list, component_array):
+        if component_max_umi[component] is None or umi_cntr[umi] > umi_cntr[component_max_umi[component]]:
+            component_max_umi[component] = umi
+    umi_map = {umi: component_max_umi[component] for umi, component in zip(umi_list, component_array)}
+    return umi_map
+
+def get_directional_umi_maps_from_bam_file(
+        bam_fpath: str,
+        chrm: str = None,
+        start: int = None,
+        end: int = None,
+        max_dist: int = 1,
+        dist_type: str = 'hamming'
+        ) -> Dict[str, Counter]:
+    """
+    Builds umi_map_given_bc for reads from (specified region of) a given file.
+
+    returns
+        :dict: umi_map_given_bc
+    """
+    umi_cntr_given_bc = defaultdict(Counter)
+    for read in pysam.AlignmentFile(bam_fpath).fetch(chrm, start, end):
+        bc = read.get_tag('CB')
+        umi = read.get_tag('UR')
+        umi_cntr_given_bc[bc][umi] += 1
+    umi_cntr_given_bc = dict(umi_cntr_given_bc)
+
+    umi_map_given_bc = {}
+    for bc, umi_cntr in umi_cntr_given_bc.items():
+        umi_map_given_bc[bc] = get_directional_umi_map_from_cntr(umi_cntr, max_dist=max_dist, dist_type=dist_type)
+
+    return umi_map_given_bc
+
